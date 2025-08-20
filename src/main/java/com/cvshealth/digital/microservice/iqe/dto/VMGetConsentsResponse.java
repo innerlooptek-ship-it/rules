572b8d9d80ece11b912dacf51f2af0e011dcb165
hareesh <mzgmehareesh@gmail.com>
2025-08-19 20:31:26 -0400

    Quetionnarie Changes

--- src/main/java/com/cvshealth/digital/microservice/iqe/dto/VMGetConsentsResponse.java
+++ src/main/java/com/cvshealth/digital/microservice/iqe/dto/VMGetConsentsResponse.java
@@ -1,23 +1,23 @@
-package com.cvshealth.digital.microservice.iqe.dto;
-
-import com.cvshealth.digital.microservice.scheduling.model.types.FaultSection;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import com.cvshealth.digital.microservice.scheduling.dto.response.ConsentData;
-
-import java.util.List;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class VMGetConsentsResponse {
-
-    private String statusCode;
-    private String statusDescription;
-    private String appointmentId;
-    private List<ConsentData> consentsData;
-    private FaultSection fault;
-}
+//package com.cvshealth.digital.microservice.iqe.dto;
+//
+//import com.cvshealth.digital.microservice.scheduling.model.types.FaultSection;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import com.cvshealth.digital.microservice.scheduling.dto.response.ConsentData;
+//
+//import java.util.List;
+//
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class VMGetConsentsResponse {
+//
+//    private String statusCode;
+//    private String statusDescription;
+//    private String appointmentId;
+//    private List<ConsentData> consentsData;
+//    private FaultSection fault;
+//}
